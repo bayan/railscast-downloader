aadd916c386c928f68bb06170bb82276b332fdaa
Bayan Khalili <bayan.net@gmail.com>
2012-09-29 13:49:38 +1000

    New minor version required due to change in interface/usage and behaviour

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject railscast-downloader "1.0.0"
+(defproject railscast-downloader "1.1.0"
   :description "Login, scrape and download videos from Railscasts"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.4.3"]
