1a5b8edcda504d2db798e9822de18030e0707976
Bayan Khalili <bayan.net@gmail.com>
2013-04-11 08:10:07 +1000

    Refactor to only use rss feed

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject railscast-downloader "1.1.0"
-  :description "Login, scrape and download videos from Railscasts"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.4.3"]
-                 [enlive "1.0.1"]]
+(defproject railscast-downloader "1.2.0"
+  :description "Download free, pro and revised screencast videos from Railscasts"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-swank "1.4.4"]]
   :main railscast-downloader.core)
--- src/railscast_downloader/core.clj
+++ src/railscast_downloader/core.clj
@@ -1,59 +1,24 @@
 (ns railscast-downloader.core
   (:gen-class)
-  (:require [clj-http.client :as client]
-            [net.cgrand.enlive-html :as enlive]
-            clojure.xml))
+  (:require clojure.xml))
 
-(declare ^:dynamic *token*)
-(declare ^:dynamic *rss-uri*)
-(declare ^:dynamic *file-type*)
+(def log
+  (let [log-agent (agent nil)]
+    (fn [& messages]
+      (send log-agent (fn [_] (apply println messages)))
+      (await log-agent))))
 
-(def root-uri "http://railscasts.com")
-(def log-agent (agent nil))
-
-(defn log
-  [& messages]
-  (send log-agent (fn [_] (apply println messages))))
-
-(defn get-as-stream
-  [uri]
-  (client/get uri {:as :stream
-                   :cookies {"token" {:path "/" :value (str *token*)}}}))
-
-(defn html-resource
-  [uri]
-  (enlive/html-resource (:body (get-as-stream uri))))
-
-(defn html-pages
-  ([] (html-pages "/"))
-  ([relative-uri]
-     (let [page (html-resource (str root-uri relative-uri))
-           next-page (first (enlive/select page [:.pagination :a.next_page]))]
-       (if next-page
-         (lazy-seq (cons page (html-pages (get-in next-page [:attrs :href]))))
-         (list page)))))
-
-(defn media-link
-  [uri media-format]
-  (let [page (html-resource uri)
-        selector [:ul.downloads :li [:a (enlive/pred #(= media-format (enlive/text %)))]]
-        link (enlive/select page selector)]
-    (-> link first :attrs :href)))
-
-(defn episode-links
-  [page]
-  (map #(media-link (str root-uri (get-in % [:attrs :href])) *file-type*)
-       (enlive/select page [:.episode :h2 :a])))
-
-(defn media-links-from-rss-feed []
-  (let [xml (-> *rss-uri*
+(defn media-links-from-rss-feed [uri file-type]
+  (let [xml (-> uri
                 slurp
                 (.getBytes "UTF-8")
                 java.io.ByteArrayInputStream.
                 clojure.xml/parse
                 xml-seq)
         xml (filter #(= :enclosure (:tag %)) xml)]
-    (map (comp :url :attrs) xml)))
+    (map #(-> % (get-in [:attrs :url])
+                (clojure.string/replace #"mp4$" file-type))
+         xml)))
 
 (defn download-media-file
   [uri]
@@ -62,21 +27,14 @@
     (if (.exists target)
       (log filename "already exists")
       (do
-        (clojure.java.io/copy (:body (get-as-stream uri)) target)
+        (clojure.java.io/copy (clojure.java.io/input-stream uri) target)
         (log filename "downloaded successfully")))))
-
-(defn download-all []
-  (let [uris (if (and *rss-uri* (= *file-type* "mp4"))
-               (media-links-from-rss-feed)
-               (mapcat episode-links (html-pages)))]
-    (doall (pmap download-media-file uris))))
 
 (defn -main
   [& args]
-  (let [arg-map (apply hash-map args)]
-    (binding [*token*     (arg-map "-token")
-              *rss-uri*   (.replace (str (arg-map "-rss")) "\"" "")
-              *file-type* (or (arg-map "-type") "mp4")]
-      (download-all))
-    (await log-agent)
+  (let [arg-map (apply hash-map args)
+        rss-uri (.replace (str (arg-map "-rss")) "\"" "")
+        file-type (or (arg-map "-type") "mp4")]
+    (doall (pmap download-media-file
+                 (media-links-from-rss-feed rss-uri file-type)))
     (shutdown-agents)))
