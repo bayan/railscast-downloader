4ae9ad4a81a24c938b52e5929d0154156283ddc4
Bayan Khalili <bayan.net@gmail.com>
2013-04-11 21:44:44 +1000

    Upgrade Clojure from 1.4.0 to 1.5.1 and remove lein-swank plugin

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject railscast-downloader "1.2.0"
   :description "Download free, pro and revised screencast videos from Railscasts"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-swank "1.4.4"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :main railscast-downloader.core)
